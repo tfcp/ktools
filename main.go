a49ceca98ade69017337f103a6d05847a8a20f22
tmaker <you@example.com>
2022-04-02 01:20:46 +0800

    debug

--- main.go
+++ main.go
@@ -1,71 +1,75 @@
 package main
 
 import (
-	"flag"
-	"github.com/ktools/cmd"
-	"github.com/ktools/library"
+	"fmt"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
+	"log"
 )
 
-const (
-	// show ktools
-	env = "env"
-	// ktools switch target env
-	sw = "switch"
-	// ktools help
-	help = "help"
-	// pipline to use ktools
-	pip = "pip"
-	// pod cmd
-	pod = "pod"
-	// ktools init
-	initCmd = "init"
-)
-
-var (
-	KConfig *library.Config
-)
-
-func init() {
-
-}
 
 func main() {
-	//flag.StringVar(&ns, "ns", "default", "input your deploy name")
-	//flag.StringVar(&resourceName, "deploy", "default", "input your deploy name")
 
-	//defer func() {
-	//	if r := recover(); r != nil {
-	//		fmt.Println("please sure your input correct...")
-	//		fmt.Println("\r\n")
-	//		cmd.Help()
-	//	}
-	//}()
+	config, err := clientcmd.BuildConfigFromFlags("", "./config/config")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	//sharedInformerFac := informers.NewSharedInformerFactory(clientset,time.Minute*10)
 
-	flag.Parse()
+	//nodeInformer := sharedInformerFac.Core().V1().Nodes()
+	//informer := nodeInformer.Informer()
+	// 初始化 informer
+	factory := informers.NewSharedInformerFactory(clientset, 0)
+	nodeInformer := factory.Core().V1().Nodes()
+	informer := nodeInformer.Informer()
+	defer runtime.HandleCrash()
+	stopper := make(chan struct{})
+	defer close(stopper)
+	// 启动 informer，list & watch
+	go factory.Start(stopper)
 
-	if len(flag.Args()) > 0 {
-		args := flag.Args()
-		if args[0] == initCmd {
-			cmd.CmdInit()
-			return
-		}
-		KConfig = library.ReadFromJson(library.GetConfigPath())
-		cmd.GlobalFmt(KConfig)
-		switch args[0] {
-		case initCmd:
-
-		case env:
-			cmd.Env(KConfig)
-		case sw:
-			envName := flag.Args()[1]
-			cmd.Switch(envName, KConfig)
-		case help:
-			cmd.Help()
-		case pip:
-			cmd.Pip()
-		case pod:
-			cmd.PodCmd()
-		}
+	// 从 apiserver 同步资源，必不可少
+	if !cache.WaitForCacheSync(stopper, informer.HasSynced) {
+		runtime.HandleError(fmt.Errorf("Timed out waiting for caches to sync"))
+		return
 	}
 
+	// 使用自定义 handler
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc:    onAdd,
+		UpdateFunc: func(interface{}, interface{}) { fmt.Println("update not implemented") }, // 此处省略 workqueue 的使用
+		DeleteFunc: func(interface{}) { fmt.Println("delete not implemented") },
+	})
+
+	// 创建 lister
+	nodeLister := nodeInformer.Lister()
+	// 从 lister 中获取所有 items
+	_, err = nodeLister.List(labels.Everything())
+	if err != nil {
+		fmt.Println(err)
+	}
+	<-stopper
+
+	select {}
 }
+
+func onAdd(obj interface{}) {
+	node := obj.(*v1.Node)
+	//fmt.Println("add a Node:", node.Name)
+	//fmt.Println("add a Cpu:", node.Status.Allocatable.Memory())
+	//fmt.Println("add a Mem:", node.Status.Allocatable.Cpu())
+	fmt.Println("add a label:", node)
+}
+
+// 参考获取Allocated resources https://www.jianshu.com/p/a3b8f1019d10

eac368c0e9bc5dfedd2492d1169b5fc9518f8ea8
tmaker <you@example.com>
2022-04-02 21:53:45 +0800

    debug

--- main.go
+++ main.go
@@ -39,18 +39,13 @@ func main() {
 	// 启动 informer，list & watch
 	go factory.Start(stopper)
 
-	// 从 apiserver 同步资源，必不可少
+	// 等待 Cache 都同步完毕,必不可少
 	if !cache.WaitForCacheSync(stopper, informer.HasSynced) {
 		runtime.HandleError(fmt.Errorf("Timed out waiting for caches to sync"))
 		return
 	}
-
 	// 使用自定义 handler
-	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc:    onAdd,
-		UpdateFunc: func(interface{}, interface{}) { fmt.Println("update not implemented") }, // 此处省略 workqueue 的使用
-		DeleteFunc: func(interface{}) { fmt.Println("delete not implemented") },
-	})
+	informer.AddEventHandler(&NodeInformerHandler{})
 
 	// 创建 lister
 	nodeLister := nodeInformer.Lister()
@@ -60,16 +55,33 @@ func main() {
 		fmt.Println(err)
 	}
 	<-stopper
-
 	select {}
 }
 
-func onAdd(obj interface{}) {
-	node := obj.(*v1.Node)
-	//fmt.Println("add a Node:", node.Name)
-	//fmt.Println("add a Cpu:", node.Status.Allocatable.Memory())
-	//fmt.Println("add a Mem:", node.Status.Allocatable.Cpu())
-	fmt.Println("add a label:", node)
+type NodeInformerHandler struct {
+
+}
+
+func (nodeInformer *NodeInformerHandler) OnAdd(obj interface{}) {
+	if _,ok := obj.(*v1.Node);ok{
+		//fmt.Println("add node:", obj.(*v1.Node).Name)
+		//fmt.Println("add node:", obj.(*v1.Node).Status.Capacity)
+		fmt.Println("add node:", obj.(*v1.Node).Spec.Unschedulable)
+		fmt.Println(nodeInformer.List(nil))
+
+	}
+}
+
+func (nodeInformer *NodeInformerHandler) OnUpdate(obj1,obj2 interface{}) {
+	if _,ok := obj1.(*v1.Node);ok{
+		fmt.Println("update node:", obj1.(*v1.Node).Name)
+	}
+}
+
+func (nodeInformer *NodeInformerHandler) OnDelete(obj interface{}) {
+	if _,ok := obj.(*v1.Node);ok{
+		fmt.Println("delete node:", obj.(*v1.Node).Name)
+	}
 }
 
 // 参考获取Allocated resources https://www.jianshu.com/p/a3b8f1019d10
